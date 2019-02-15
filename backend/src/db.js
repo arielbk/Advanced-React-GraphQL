fd3a6313a7f42a7ae46ee2e91343045401cadfd2
arielbk <buchwald.kearney@gmail.com>
2019-02-15 17:28:24 +0100

    Prisma and Yoga initial

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// Connects to remote prisma db so we can query with js
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.export = db;
